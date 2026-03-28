59204d3861a5ee78ab126d603c5069e65d1eec50
Naimul-2019331065 <nhnahin65@gmail.com>
2026-03-28 23:13:59 +0600

    frontend hero section done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import AiTools from "../components/AiTools";
+import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
     return (
-        <>
+        <div>
             <Navbar/>
-        </>
+            <Hero/>
+            <AiTools/>
+        </div>
     );
 };
 
